7bf39249da7385265b70e40bf03851780e1acde7
sskerr <sarah.s.kerr@gmail.com>
2017-04-27 18:24:28 -0400

    Functional filter and some styling

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
 $(document).ready(function () {
 	$(".filter").on("click", function (e) {
-		e.preventDefault();
 
-		var button = $(this);
-		var filter = button.data("filter");
+		$(".item.active").removeClass("active");
+		$("[type='checkbox']:checked").each(function () {
+			var checkbox = $(this),
+				filter = checkbox.data("filter");
+			$("." + filter).addClass("active");
+		});
 
 
-		if (filter === "all") {
-			$(".item").addClass("active");
+		$("#all").on("click", function (e) {
+			if (this.checked) {
+				$(":checkbox").each(function () {
+					this.checked = true;
+				});
+			} else {
+				$(':checkbox').each(function () {
+					this.checked = false;
+				});
+			}
 
-		} else {
-			/*$(".item.active").removeClass("active");*/
-			$(".item" + "." + filter).addClass("active");
-		}
+		});
 
-		if (filter === "reset") {
-			$(".item").removeClass("active");
-		}
+		$("#reset").on("click", function (e) {
+			$(":checkbox").each(function () {
+				this.checked = false;
+				$(".item.active").removeClass("active");
+			});
+		})
 
 	});
 
-	/*function toggle("button") {
-		if
-		$(this).val("click").addClass("btn_active");
-	}
 
-	});
-	*/
+
+	/*item").addClass("active");
+	console.log("hello world")*/
 
 });
+
+
+
+
+
+
+/*
+
+	if (filter === "reset") {
+		$(".item").removeClass("active");
+	} else {
+		$(".item.active").addClass("active");
+		$(".item" + "." + filter).addClass("active");
+	}
+
+	/*if (filter === "all") {
+			$(".item").addClass("active");
+
+		}
+*/
